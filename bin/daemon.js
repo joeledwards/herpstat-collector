463752a51945a07b69ae4529345d3f19ab4374e2
Joel Edwards <joeledwards@gmail.com>
2024-04-18 00:31:44 -0600

    feat: added --check-interval and inner loop

--- bin/daemon.js
+++ bin/daemon.js
@@ -11,19 +11,25 @@ const options = yargs
     type: 'string',
     desc: 'The id to use when saving metrics for this device',
     default: 'herpstat',
-    alias: ['i', 'id'],
+    alias: ['id'],
   })
   .option('herpstat-device-url', {
     type: 'string',
     desc: 'The URL of the Herpstat device from which to collect metrics',
     default: 'http://herpstat:8080',
-    alias: ['u', 'url'],
+    alias: ['url'],
   })
   .option('redis-server', {
     type: 'string',
     desc: 'The Redis server to which metrics should be saved',
     default: 'redis://localhost:6379',
-    alias: ['r', 'redis'],
+    alias: ['redis'],
+  })
+  .option('check-interval', {
+    type: 'number',
+    desc: 'Seconds between performing checks (minimum is 15)',
+    default: 60,
+    alias: ['interval'],
   })
   .parse()
 
