dce7383d3ec1b83487f11b5e3deb679926a0bc86
Joel Edwards <joeledwards@gmail.com>
2024-04-18 00:02:32 -0600

    fix: corrected the path and request to the herpstat JSON API

--- bin/daemon.js
+++ bin/daemon.js
@@ -7,20 +7,23 @@ const collector = require('../lib/collector')
 
 const options = yargs
   .env('HERPSTAT_COLLECTOR')
-  .option('herpstat-device-alias', {
+  .option('herpstat-device-id', {
     type: 'string',
-    desc: 'The address of the Herpstat device from which to collect metrics',
+    desc: 'The id to use when saving metrics for this device',
     default: 'herpstat',
+    alias: ['i', 'id'],
   })
   .option('herpstat-device-url', {
     type: 'string',
     desc: 'The URL of the Herpstat device from which to collect metrics',
     default: 'http://herpstat:8080',
+    alias: ['u', 'url'],
   })
   .option('redis-server', {
     type: 'string',
     desc: 'The Redis server to which metrics should be saved',
     default: 'redis://localhost:6379',
+    alias: ['r', 'redis'],
   })
   .parse()
 
