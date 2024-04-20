fb57d5beabf193230e144f1b2e74ce7433ce0551
Joel Edwards <joeledwards@gmail.com>
2024-04-19 23:48:22 -0600

    feat: correct dataDir; cleanup

--- bin/daemon.js
+++ bin/daemon.js
@@ -10,7 +10,6 @@ const options = yargs
   .option('device-alias', {
     type: 'string',
     desc: 'The alias to use when saving metrics for this device',
-    default: 'herpstat',
     alias: ['alias'],
   })
   .option('device-url', {
@@ -22,10 +21,9 @@ const options = yargs
   .option('redis-uri', {
     type: 'string',
     desc: 'The Redis server to which metrics should be saved',
-    default: 'redis://localhost:6379',
     alias: ['redis'],
   })
-  .option('data-path', {
+  .option('data-dir', {
     type: 'string',
     desc: 'The directory where full records should be written',
   })
